af5cbddc548e71d213498aed2dd48f6fd3126cc2
Miguel Dorado <doradomiguel35@gmail.com>
2019-02-06 16:08:08 +0800

    dynamic star rating, update star rating of the theme after every created review

--- details/views.py
+++ details/views.py
@@ -8,24 +8,52 @@ from .serializers import ReviewSerializer
 from rest_framework.authtoken.models import Token
 
 
+
 class CreateReview(APIView):
 	"""create a review on a post
 	"""
 	permission_classes = (AllowAny,)
-	
-	def post(self,request,*args,**kwargs):
-		token = self.request.data.get('token')
+	sum_values = 0
 
-		self.request.data['user'] = Token.objects.get(key=token).user_id
-		self.request.data['theme'] = self.request.data.get('theme_id')
+	def post(self,request,*args,**kwargs):
+		"""
+		"""
+		token = request.data.get('token')
+
+		request.data['user'] = Token.objects.get(key=token).user_id
+		request.data['theme'] = request.data.get('theme_id')
 		
 		serializer = ReviewSerializer(
-			data=self.request.data)
+			data=request.data)
 		
+		"""save review after validation
+		"""
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
-		
+
+		"""get average rating of the theme between the ratings of the reviews
+		"""
+		theme_rating = Theme.objects.get(id=request.data.get('theme_id'))
+		reviews_rating = Review.objects.filter(theme_id=request.data.get('theme_id')).values('rating')
+		list_values = list(reviews_rating)
+		average_rating = self.get_average_rating(list_values,'rating')
+
+		"""update theme with new rating
+		"""
+		theme_rating.rating = int(average_rating)
+		theme_rating.save()
+
 		return Response({'success': 'Review created!'}, status=200)
+
+	def get_average_rating(self,list_values,key):
+		for rating in list_values:
+			self.sum_values += rating[key]
+		return self.sum_values/len(list_values)
+
+
+
+
+
 
 		
 
