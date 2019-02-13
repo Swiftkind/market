0553d478326a7147445527b954eb8a2b238e897e
Miguel Dorado <doradomiguel35@gmail.com>
2019-02-13 11:18:53 +0800

    forget password feature will now send a unique link via Gmail for password resetting

--- market/settings.py
+++ market/settings.py
@@ -159,8 +159,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets/'),
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'   
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bambolino35@gmail.com'
+EMAIL_HOST_PASSWORD = 'fqu8moex'
+DEFAULT_FROM_EMAIL= EMAIL_HOST_USER
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 # Allow any settings to be defined in local_settings.py which should be
 # ignored in your version control system allowing for settings to be
 # defined per machine.
